3131175322819404f60b404ec6be699b2264540f
181710062 <181710062@LAB30817.senacinformatica.br>
2018-12-10 09:58:11 -0200

    Merge origin/master
    Conflicts:
    	src/telas/TelaEstoque.java

--- enviar.php
+++ enviar.php
@@ -1,6 +1,6 @@
 <?php
 $con = mysql_connect('localhost','root','') or die (mysql_error());
-$x1 = mysql_select_db('chat', $con) or die (mysql_error());
+$x1 = mysql_select_db('celke', $con) or die (mysql_error());
 ?>
 
 <?php
@@ -8,7 +8,5 @@ $usuario=$_POST['usuario'];
 $mensagem=$_POST['mensagem'];
 
 $sql =@mysql_query("INSERT INTO mensagens(usuario,mensagem) VALUES ('$usuario','$mensagem')") or die (mysql_error());
-if($sql >0) {
 
-}
 ?>
